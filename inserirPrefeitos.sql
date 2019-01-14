c610ae5446acb7e3262e395cb0ac95955d9d63b5
Matheus Zanetti <matheus.zanetti@intelliway.com.br>
2019-01-14 14:19:05 -0200

    inserting via different values

---
+++ inserirPrefeitos.sql
@@ -0,0 +1,12 @@
+select * from cidades;
+
+INSERT INTO prefeitos (nome, cidade_id)
+VALUES
+    ('Rodrigo Neves', 2),
+    ('Raquel Lyra', 3),
+    ('Zenaldo Coutinho', null);
+
+select * from prefeitos;
+
+INSERT INTO prefeitos (nome, cida_id)
+VALUES
